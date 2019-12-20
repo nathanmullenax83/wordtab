8c780540247c7b27fecae5fc12b27c37c9af5c34
Nathan Mullenax <nathanmullenax83@github.com>
2019-12-20 01:18:01 +0000

    Added CLI for generating markov rule tables.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
     Formatter console;
 
     vector<string> args = console.parse_args(argc, argv);
-    console.print(args);
+    
 
     
     // parse arguments
@@ -29,7 +29,18 @@ int main(int argc, char **argv) {
         if( args[4]=="text" ) {
             size_t length = console.parse<size_t>( args[5] );
             console.print( dict.decode(seq.generate(model,length)));
+        } 
+    } else if( args.size() == 5 && args[1]=="markov" ) {
+        size_t prefix_length = console.parse<size_t>(args[2]);
+        size_t suffix_length = console.parse<size_t>(args[3]);
+        size_t model = seq.define_model( prefix_length, suffix_length );
+        console.read( dict, seq, cin );
+        if( args[4]=="table") {
+            seq.dump(model);
         }
+    } else {
+        cout << "Didn't recognize:\n\n";
+        console.print(args);
     }
 
 }
